9cab7bee6f301568a3657e7dc79676f7e8a47f33
unknown <louis.nguyen8022@gmail.com>
2022-09-08 16:01:47 +0700

    3 commit

--- main.js
+++ main.js
@@ -2,19 +2,23 @@ var mobileBtn = document.querySelectorAll(".mobile-menu-btn")[0];
 var mobileMenu = document.querySelectorAll(".nav-bar")[0]
 var subMenu = document.querySelectorAll(".sub-nav")[0]
 var mainMenu = document.querySelectorAll(".nav-bar")[0]
+var upIcon = document.getElementById("i-up")
+var downIcon = document.getElementById("i-down")
 var menuHeight = mobileMenu.offsetHeight;
 
 mobileBtn.onclick =  () =>{
     var menuHeight = mobileMenu.offsetHeight;
-    menuHeight === 46 ? mobileMenu.style.height = "368px" :  mobileMenu.style.height = "46px"
+    menuHeight === 46 ? mobileMenu.style.height = "100vh" :  mobileMenu.style.height = "46px"
 
-//    if(menuHeight === 46){
-//         mobileMenu.style.height = "368px"
-//         // subMenu.style.display = "block"
-//    } else{
-//         mobileMenu.style.height = "46px"
-//         // subMenu.style.display = "none"
-//     }
+   if(menuHeight === 46){
+        mobileMenu.style.height = "100vh"
+       upIcon.style.display = "block"
+       downIcon.style.display = "none"
+   } else{
+        mobileMenu.style.height = "46px"
+        upIcon.style.display = "none"
+       downIcon.style.display = "block"
+    }
 };
 window.onscroll = () =>{
   
@@ -25,11 +29,14 @@ window.onscroll = () =>{
     //         mobileMenu.style.width = "90%"
     //     }
     // }
-    if( window.pageXOffset < 740 && window.pageYOffset < 46){
-        mobileMenu.style.width = "90%"
-    } else{
-        mobileMenu.style.width = "100%"
-        mainMenu.style.width = "100%"
-    }
+
+
+
+    // if( window.pageXOffset < 740 && window.pageYOffset < 46){
+    //     mobileMenu.style.width = "90%"
+    // } else{
+    //     mobileMenu.style.width = "100%"
+    //     mainMenu.style.width = "100%"
+    // }
     
 }
